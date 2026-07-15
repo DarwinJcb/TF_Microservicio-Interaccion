bfffaa4814573201d2eeb443b78cc0f2a0aec1d9
Darwin <djcabrera@sudamericano.edu.ec>
2026-07-15 13:18:09 -0500

    Commit #22

--- src/app.service.ts
+++
@@ -1,9 +0,0 @@
-/* tf_microservicio-interacciones/src/app.service.ts */
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return '¡Hola Mundo!';
-  }
-}
